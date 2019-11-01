caff3e7a03c6638097e1f9fe4e8c545b009a5a20
Loki <zpk.loki@gmail.com>
2019-11-01 18:12:36 +0300

    js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI Elements
@@ -17,6 +17,14 @@ const game = document.querySelector("#game"),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+//Play Again event listener
+game.addEventListener('mousedown', function(e){
+if(e.target.className === 'play-again'){
+window.location.reload();
+}
+});
+
+
 //Listen for guess
 
 guessBtn.addEventListener("click", function() {
@@ -29,16 +37,58 @@ guessBtn.addEventListener("click", function() {
 
   // Check if won
   if (guess === winningNum) {
-    // Disable input
-    guessInput.disabled = true;
-    // Change border color
-    guessInput.style.borderColor = "green";
-    // Set message
-    setMessage(`${winningNum} is correct, YOU WIN!`, "green");
+    // Game Over -Won
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
   } else {
-    
+    // Wrong number
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      // Game Over -lost
+
+      gameOver(
+        false,
+        `Game Over, you lost. The correct number was ${winningNum}`
+      );
+    } else {
+      // Game continues - answer wrong
+
+      // Change border color
+      guessInput.style.borderColor = "red";
+      //Clear input
+      guessInput.value = "";
+      //Tell user its the wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, "red");
+    }
   }
 });
+
+
+
+//Game over
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+
+  // Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
+
+  //Play again
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
+}
+
+//Get winning number
+
+function getRandomNum(min, max) {
+
+}
 
 //Set message
 function setMessage(msg, color) {
